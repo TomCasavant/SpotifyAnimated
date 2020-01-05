4ed427efbfb17787017ac69cb18ab2cf7e03ec92
Tom Casavant <tfcasavant@gmail.com>
2020-01-05 16:18:07 -0500

    Created AJAX request to get chart

--- app.py
+++ app.py
@@ -1,9 +1,12 @@
 import io
 from spotifyauth import startup
 from flask import Flask, render_template, redirect, request, url_for, session, Response
+from flaskthreads import AppContextThread
 
 from src.plotter import gen_plot
 from src.spotify import get_playlists
+
+from flask import jsonify, json
 
 app = Flask(__name__)
 
@@ -14,6 +17,10 @@ def index():
         # Get all the playlists for the authenticated user
         playlists = get_playlists(startup.getAccessToken()[0])
 
+    else:
+        # User is not authenticated, authenticate user with spotify
+        response = startup.getUser()
+        return redirect(response, code=302)
     if (request.method == 'POST'):
         # Graph Creation
         choice = request.form.get('playlists') # Get selected playlist
@@ -22,9 +29,8 @@ def index():
         title = request.form.get('title')
         freq = int(request.form.get('increment'))
         song_count = int(request.form.get('song_count'))
-        chart = gen_plot(startup.getAccessToken()[0], choice, background_color, title, text_color, freq, song_count) # Create the plot
-        return Response(gen_plot(startup.getAccessToken()[0], choice, background_color, title, text_color, freq, song_count), mimetype="text/html")
-        #return render_template('chart.html', playlists=playlists, chart=chart) # Render the plot
+        #chart = gen_plot(startup.getAccessToken()[0], choice, background_color, title, text_color, freq, song_count) # Create the plot
+        return render_template('chart.html', playlists=playlists) # Render the plot
 
     else:
         if auth:
@@ -34,6 +40,19 @@ def index():
             # User is not authenticated, authenticate user with spotify
             response = startup.getUser()
             return redirect(response, code=302)
+@app.route('/_generate')
+def generate():
+    print(request.args.get('a'))
+    choice = request.args.get('playlist') # Get selected playlist
+    freq = int(request.args.get('freq'))
+    song_count = int(request.args.get('song_count'))
+    text_color = request.args.get('text_color')
+    background_color = request.args.get('background_color')
+    title = request.args.get('title')
+
+    chart = gen_plot(startup.getAccessToken()[0], choice, background_color, title, text_color, freq, song_count) # Create the plot
+    print(chart)
+    return jsonify(chart=chart)
 
 @app.route('/callback/')
 def callback():
